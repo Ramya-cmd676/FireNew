1e3cc5205341a268c973b80cfaceae978633630d
Ramya-cmd676 <2022cs_ramyakm_c@nie.ac.in>
2025-05-31 10:09:54 +0530

    xyz

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -22,7 +22,7 @@ const [password, setPassword] = useState('');
   }
 
   try {
-    const response = await fetch('http://localhost:5000/api/login', {
+    const response = await fetch('https://firenew.onrender.com/api/login', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
